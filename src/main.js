00cc501a010e9fddac79be75602b17d258ce1835
majid <majid.eltayeb@gmail.com>
2020-09-11 19:21:20 +0200

    :bug: deploy bug

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import Vue from "vue";
-import App from "./app.vue";
+import App from "./App.vue";
 import { router } from "@router";
 import VueYouTubeEmbed from "vue-youtube-embed";
 
